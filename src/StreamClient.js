d8a9f00b1c7105ff7180b2753f8379a9e745b71a
Jo Voordeckers <jo.voordeckers@gmail.com>
2014-08-20 16:53:18 -0700

    Multiplexing of stream subscriptions Rewinding of streams in case of out of order messages

--- src/StreamClient.js
+++ src/StreamClient.js
@@ -23,7 +23,7 @@ function mkLoggerIfNeeded(level) {
             }
         }
     } else if (console && console.log) {
-        var prefix = level.toUpperCase()+": "
+        var prefix = level.toUpperCase()+": ";
         return function() {
             console.log(prefix + [].slice.call(arguments).join(" "));
         }
@@ -35,7 +35,7 @@ var logger = {
     log: mkLoggerIfNeeded("log"),
     warn: mkLoggerIfNeeded("warn"),
     error: mkLoggerIfNeeded("error")
-}
+};
 
 var States = {
     DISCONNECTED : "DISCONNECTED",
@@ -46,25 +46,25 @@ var States = {
     STREAMING : "STREAMING",
     REBALANCING : "REBALANCING",
     ERROR : "ERROR"
-}
+};
 
 var environments = {
     qa: 'stream.qa-ext.livefyre.com',
     uat: 'stream.t402.livefyre.com',
     production: 'stream.livefyre.com'
-}
+};
 
 var State = function State(initialState) {
     EventEmitter.call(this);
     this.value = initialState || States.DISCONNECTED;
-}
+};
 State.prototype.change = function change(state) {
-    var old = this.value
+    var old = this.value;
     if (old !== state) {
-        this.value = state
+        this.value = state;
         this.emit('change', old, state);
     }
-}
+};
 extend(State.prototype, EventEmitter.prototype);
 
 /**
@@ -84,7 +84,7 @@ function StreamClient(options) {
     if (this.options.protocol.slice(-1) !== ':') {
         this.options.protocol += ':';
     }
-    this.options.port = Number(this.options.port)
+    this.options.port = Number(this.options.port);
     if (!this.options.port) {
         if (this.options.protocol === "http:") {
             this.options.port = 80
@@ -94,7 +94,7 @@ function StreamClient(options) {
             throw new Error("Invalid protocol and port");
         }
     }
-    this.options.endpoint = this.options.endpoint || '/stream'
+    this.options.endpoint = this.options.endpoint || '/stream';
     if (!this.options.hostname && options.environment) {
         this.options.hostname = environments[options.environment];
     }
@@ -104,8 +104,6 @@ function StreamClient(options) {
 
     // SockJS - Stream Connection
     this.lfToken = null;
-    this.streamId = null;
-    this.sessionId = null;
     this.conn = null;
     this.lastError = null;
     this.retryCount = 0;
@@ -113,46 +111,25 @@ function StreamClient(options) {
     this.allProtocols = Object.keys(SockJS.getUtils().probeProtocols());
     this.allProtocolsWithoutWS = this.allProtocols.slice();
     this.allProtocolsWithoutWS.splice(this.allProtocols.indexOf("websocket"),1);
-
-
+    this.streams = {};
     // Stream State
     this.States = States; // Make accessible for testing
     this.state = new State(States.DISCONNECTED);
     this.state.on("change", this._stateChangeHandler.bind(this));
-
-    // ReadableStream.pipe support
-    this.pipeHandlers = [];
-    this._setupPipeHandlers('data');
-    this._setupPipeHandlers('end');
 }
 extend(StreamClient.prototype, EventEmitter.prototype);
 
 /**
  * @private
  */
-StreamClient.prototype._setupPipeHandlers = function _setupPipeHandlers(handlerType) {
-    this.on(handlerType, function(data){
-        this.pipeHandlers.forEach(function(pipe) {
-            try {
-                pipe[handlerType](data);
-            } catch (e) {
-                logger.error("StreamClient: Error calling handler for", handlerType, e)
-            }
-        }.bind(this));
-    }.bind(this))
-}
-
-/**
- * @private
- */
 StreamClient.prototype._stateChangeHandler = function _stateChangeHandler(oldState, newState) {
-    if (this.options.debug) logger.debug("DEBUG", oldState, ">>", newState)
+    if (this.options.debug) logger.debug("DEBUG", oldState, ">>", newState);
     if (newState == States.CONNECTING || newState == States.RECONNECTING) {
         var connect = function () {
             var opts = {
                 debug: this.options.debug,
                 protocols_whitelist: this.rebalancedTo ? this.allProtocols : this.allProtocolsWithoutWS
-            }
+            };
             this.conn = new SockJS(this._streamUrl(), undefined, opts);
             this._connectListeners();
             this.retryCount++;
@@ -172,9 +149,10 @@ StreamClient.prototype._stateChangeHandler = function _stateChangeHandler(oldSta
     if (newState == States.DISCONNECTED) {
         this.conn = null;
         if (oldState == States.DISCONNECTING) {
-            this.sessionId = null;
-        }
-        if (oldState == States.DISCONNECTING || oldState == States.ERROR) {
+            Object.keys(this.streams).forEach(function(urn){
+                var subscription = this.streams[urn];
+                subscription.close();
+            }.bind(this))
             this.emit("end");
             this.emit("close");
         } else if (oldState == States.REBALANCING) {
@@ -199,16 +177,10 @@ StreamClient.prototype._stateChangeHandler = function _stateChangeHandler(oldSta
         }
     }
     if (newState == States.CONNECTED) {
-        this._sendControlMessage({
-            action: "subscribe",
-            hostname: this._streamHost(),
-            lfToken: this.lfToken,
-            streamId: this.streamId,
-            sessionId: this.sessionId
-        });
+        this._ensureSubscriptionState();
     }
     if (newState == States.REBALANCING) {
-        logger.log("Rebalancing to Stream:", this.streamId, "at", this._streamUrl());
+        logger.log("Rebalancing for Streams:", Object.keys(this.streams), "at", this._streamUrl());
         this.retryCount = 0;
     }
     if (newState == States.STREAMING) {
@@ -223,7 +195,36 @@ StreamClient.prototype._stateChangeHandler = function _stateChangeHandler(oldSta
             this.state.change(States.DISCONNECTED);
         }
     }
-}
+};
+
+StreamClient.prototype._ensureSubscriptionState = function _ensureSubscriptionState() {
+    var keys = Object.keys(this.streams);
+    if (keys.length == 0 && this.state.value != States.DISCONNECTED) {
+        this.state.change(States.DISCONNECTING);
+    } else if (keys.length > 0 && this.state.value == States.DISCONNECTED) {
+        this.state.change(States.CONNECTING);
+    } else if (this.state.value == States.CONNECTED || this.state.value == States.STREAMING) {
+        this._sendControlMessage({
+            action: "subscribe",
+            hostname: this._streamHost(),
+            lfToken: this.lfToken,
+            streams: this._streamsJSON()
+        });
+    }
+};
+
+/**
+ * @returns [Object] the streams section of the subscription message (array)
+ * @private
+ */
+StreamClient.prototype._streamsJSON = function _streamsJSON() {
+    var streamsJSON = [];
+    Object.keys(this.streams).forEach(function(urn){
+        var subscription = this.streams[urn];
+        streamsJSON.push(subscription._formatJSON());
+    }.bind(this));
+    return streamsJSON;
+};
 
 /**
  * @returns {String} the composed stream URL
@@ -233,7 +234,7 @@ StreamClient.prototype._streamUrl = function _streamUrl() {
     // don't append :80 for http, IE trips over that
     var portSuffix = ((this.options.protocol === "http:" && this.options.port === 80) ? '' : ':' + this.options.port);
     return this.options.protocol + '//' + this._streamHost() + portSuffix + this.options.endpoint;
-}
+};
 
 /**
  * @returns {String} the stream host
@@ -241,35 +242,42 @@ StreamClient.prototype._streamUrl = function _streamUrl() {
  */
 StreamClient.prototype._streamHost = function _streamHost() {
     return (this.retryCount < 3 && this.rebalancedTo) || this.options.hostname;
-}
+};
 
 /**
  * @private
  */
 StreamClient.prototype._onControlMessage = function _onControlMessage(message) {
     if (message.action == "subscribed") {
-        this.sessionId = message.sessionId;
         this.state.change(States.STREAMING);
     }
     if (message.action == "rebalance") {
         this.rebalancedTo = message.hostname;
         this.state.change(States.REBALANCING);
     }
+    if (message.action == "rewindFailed") {
+        logger.warn("StreamClient failed to rewind stream:", message.streamId);
+        var stream = this.streams[message.streamId];
+        if (stream) {
+            stream._onError(message.error);
+        }
+    }
     if (message.action == "error") {
         logger.error("StreamClient error, disconnecting, reason:", message.error);
         this.lastError = new Error(message.error);
         this.state.change(States.ERROR);
     }
-}
+};
 
 /**
  * @private
  */
 StreamClient.prototype._sendControlMessage = function _sendControlMessage(message) {
     if (!this.conn) {
-        throw new Error("Can't send control message, not connected");
+        throw new Error("Can't send control message, not connected: " + this.state.value);
     }
     var ctrlMsg = {
+        protocol: "V1",
         topic: "control",
         body: message
     };
@@ -278,23 +286,40 @@ StreamClient.prototype._sendControlMessage = function _sendControlMessage(messag
 }
 
 /**
- * Connects to the Stream.
  * @param {LFToken} lfToken - the LF-Token which contains the JID to be used
- * @param {String} streamId - the ID of the stream
  */
-StreamClient.prototype.connect = function connect(lfToken, streamId) {
-    if (this.conn) {
-        throw new Error("StreamClient already connected");
-    }
-    if (!(lfToken && streamId)) {
-        throw new Error("lfToken and streamId are mandatory");
-    }
-    logger.log("Connecting to Stream:", streamId, "at", this._streamUrl())
+StreamClient.prototype.auth = function auth(lfToken) {
     this.lfToken = lfToken;
-    this.streamId = streamId;
-    this.rebalancedTo = null;
-    this.state.change(States.CONNECTING);
 }
+
+StreamClient.prototype._truncUrnClassifier = function _truncUrnClassifier(urn) {
+    return urn.replace(/:(topicStream|personalStream|collectionStream)/, "");
+}
+
+/**
+ * @param urn Full URN with classifier of the stream
+ * @param eventId
+ * @returns {StreamSubscription}
+ */
+StreamClient.prototype.subscribe = function subscribe(urn, eventSequence, eventId) {
+    logger.log("Subscribing to Stream:", urn, "at", this._streamUrl())
+    var urnUnClassified = this._truncUrnClassifier(urn);
+    var streamSubscription = new StreamSubscription(this, urn, eventSequence, eventId);
+    this.streams[urnUnClassified] = streamSubscription;
+    this.rebalancedTo = null;
+    this._ensureSubscriptionState();
+    return streamSubscription;
+};
+
+/**
+ * @param urn Full URN with classifier of the stream
+ */
+StreamClient.prototype._unsubscribe = function _unsubscribe(urn) {
+    logger.log("Unsubscribing from Stream:", urn);
+    var urnUnClassified = this._truncUrnClassifier(urn);
+    delete this.streams[urnUnClassified];
+    this._ensureSubscriptionState();
+};
 
 /**
  * @private
@@ -322,32 +347,122 @@ StreamClient.prototype._connectListeners = function _connectListeners() {
             if (self.state.value != States.STREAMING) {
                 logger.warn("Unexpected stream message for state ", self.state.value);
             }
+            var stream = self.streams[msg.streamId];
+            stream && stream._onMessage(msg.sequence, msg.eventId, msg.body);
             self.emit("data", msg.body);
         } else {
             self.emit("error", new Error("Unsupported message received: " + JSON.stringify(msg)));
         }
     }
-}
+};
 
 /**
  * Disconnects from the stream, closing the session on the stream server.
  */
 StreamClient.prototype.disconnect = function disconnect() {
     if (!this.conn) {
-        throw new Error("StreamClient not connected")
+        logger.warn("StreamClient not connected")
+    } else {
+        logger.log("Disconnecting from Stream at ", this._streamUrl());
+        this.state.change(States.DISCONNECTING);
     }
-    logger.log("Disconnecting from Stream at ", this._streamUrl());
-    this.state.change(States.DISCONNECTING);
+};
+
+function StreamSubscription(client, streamId, eventSequence, eventId) {
+    this._client = client;
+    this.streamId = streamId;
+    this.sequence = eventSequence === undefined ? null : eventSequence;
+    this.eventId = eventId === undefined ? null : eventId;
+
+    // ReadableStream.pipe support
+    this.paused = false;
+    this._listeners = []; // Bug in event-emitter
+    this.pipeHandlers = [];
+    this._setupPipeHandlers('data');
+    this._setupPipeHandlers('end');
 }
+extend(StreamSubscription.prototype, EventEmitter.prototype);
+
+StreamSubscription.prototype._onError = function _onError(errorMessage) {
+    this.sequence = null;
+    this.eventId = null;
+    this.emit('error', new Error(errorMessage));
+};
+
+StreamSubscription.prototype._onMessage = function _onMessage(sequence, eventId, messageBody) {
+    if (this.sequence === null || sequence === this.sequence + 1) {
+        // normal sequence
+        this.sequence = sequence;
+        this.eventId = eventId;
+        this.emit('data', {
+            eventSequence: sequence,
+            eventId: eventId,
+            event: messageBody
+        });
+    } else if (sequence <= this.sequence) {
+        // ignore repeated values
+        logger.warn("Dropping repeated message seq:", sequence, "id:", eventId);
+    } else {
+        logger.error("Message(s) lost in flight, rewinding from seq:", this.sequence);
+        this._client._sendControlMessage({
+            action: "rewind",
+            host: this._client._streamHost(),
+            lfToken: this._client.lfToken,
+            streams: [
+                this._formatJSON()
+            ]
+        });
+    }
+};
+
+StreamSubscription.prototype._formatJSON = function _formatJSON() {
+    return {
+        streamId: this.streamId,
+        resumeTime: this.eventId,
+        resumeSeq: this.sequence
+    };
+}
+
+StreamSubscription.prototype.close = function close() {
+    this.emit('end');
+    this._client._unsubscribe(this.streamId);
+};
 
 // === begin === node stream.Readable API
 
-StreamClient.prototype.read = function read(size) { throw new Error("Not implemented"); }
-StreamClient.prototype.setEncoding = function setEncoding(encoding) { throw new Error("Not implemented"); }
-StreamClient.prototype.resume = function resume() { throw new Error("Not implemented"); }
-StreamClient.prototype.pause = function pause() { throw new Error("Not implemented"); }
-StreamClient.prototype.pipe = function pipe(dest, opts) {
-    opts = opts || { end: true }
+StreamSubscription.prototype._setupPipeHandlers = function _setupPipeHandlers(handlerType) {
+    var buffer = [];
+    var writeOut = function writeOut(data) {
+        this.pipeHandlers.forEach(function(pipe) {
+            try {
+                pipe[handlerType](data);
+            } catch (e) {
+                logger.error("StreamClient: Error calling handler for", handlerType, e)
+            }
+        }.bind(this));
+    }.bind(this);
+    this.on("resumed", function resume(){
+        while(buffer.length > 0) {
+            writeOut(buffer.shift())
+        }
+    });
+    this.on(handlerType, function(data){
+        if (this.paused) {
+            buffer.push(data);
+        } else {
+            writeOut(data);
+        }
+    }.bind(this));
+};
+StreamSubscription.prototype.resume = function resume() {
+    this.emit("resumed");
+    this.paused = false;
+};
+StreamSubscription.prototype.pause = function pause() {
+    this.paused = true;
+};
+StreamSubscription.prototype.pipe = function pipe(dest, opts) {
+    opts = opts || { end: true };
     var pipeHandler = {
         dest: dest,
         end: function() {
@@ -360,11 +475,11 @@ StreamClient.prototype.pipe = function pipe(dest, opts) {
         }
     };
     this.pipeHandlers.push(pipeHandler);
-    return dest
-}
-StreamClient.prototype.unpipe = function unpipe(dest) {
+    return dest;
+};
+StreamSubscription.prototype.unpipe = function unpipe(dest) {
     if (!dest) {
-        this.pipeHandlers = []
+        this.pipeHandlers = [];
     } else {
         var pipeIdx;
         for (var i = 0; i < this.pipeHandlers.length; i++) {
@@ -377,6 +492,10 @@ StreamClient.prototype.unpipe = function unpipe(dest) {
         }
         this.pipeHandlers.splice(pipeIdx, 1);
     }
-}
-StreamClient.prototype.unshift = function unshift(chunk) { throw new Error("Not implemented"); }
-StreamClient.prototype.wrap = function wrap(stream) { throw new Error("Not implemented"); }
+};
+StreamSubscription.prototype.read = function read(size) { throw new Error("Not implemented"); };
+StreamSubscription.prototype.setEncoding = function setEncoding(encoding) { throw new Error("Not implemented"); };
+StreamSubscription.prototype.unshift = function unshift(chunk) { throw new Error("Not implemented"); }
+StreamSubscription.prototype.wrap = function wrap(stream) { throw new Error("Not implemented"); }
+
+// === end === node stream.Readable API
